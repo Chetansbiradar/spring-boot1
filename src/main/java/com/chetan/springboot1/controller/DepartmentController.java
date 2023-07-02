6bc40d7be013861ce9a8b476be8c308cd4828c0d
Chetan P Hattoor <chetanhattoor@gmail.com>
2023-07-02 18:14:09 +0530

    removed new Dept();

--- src/main/java/com/chetan/springboot1/controller/DepartmentController.java
+++ src/main/java/com/chetan/springboot1/controller/DepartmentController.java
@@ -1,8 +1,21 @@
 package com.chetan.springboot1.controller;
 
+import com.chetan.springboot1.entity.Department;
+import com.chetan.springboot1.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DepartmentController {
 
+    @Autowired
+    private DepartmentService departmentService;
+
+    @PostMapping("/departments")
+    public Department saveDepartment(@RequestBody Department department){
+        System.out.println(department);
+        return departmentService.saveDepartment(department);
+    }
 }
