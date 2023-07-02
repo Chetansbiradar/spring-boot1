7af5b9bce676a6c4f4322ca06a9063ba94f24081
Chetan P Hattoor <chetanhattoor@gmail.com>
2023-07-02 18:13:16 +0530

    saveDepartment added

--- src/main/java/com/chetan/springboot1/service/DepartmentService.java
+++ src/main/java/com/chetan/springboot1/service/DepartmentService.java
@@ -1,4 +1,7 @@
 package com.chetan.springboot1.service;
 
+import com.chetan.springboot1.entity.Department;
+
 public interface DepartmentService {
+    Department saveDepartment(Department department);
 }
