117b168989f13372f151ed3488e8f911475aac9d
Chetan P Hattoor <chetanhattoor@gmail.com>
2023-07-02 16:13:14 +0530

    iDepartment Repository

---
+++ src/main/java/com/chetan/springboot1/repository/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.chetan.springboot1.repository;
+
+import com.chetan.springboot1.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department,Long> {
+}
