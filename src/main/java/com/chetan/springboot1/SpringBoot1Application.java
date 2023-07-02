2baf1993c126751c36159beb74c783183b73e5d4
Chetan P Hattoor <chetanhattoor@gmail.com>
2023-07-02 16:13:58 +0530

    main app

---
+++ src/main/java/com/chetan/springboot1/SpringBoot1Application.java
@@ -0,0 +1,15 @@
+package com.chetan.springboot1;
+
+import com.chetan.springboot1.entity.Department;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot1Application.class, args);
+		Department department = new Department();
+	}
+
+}
