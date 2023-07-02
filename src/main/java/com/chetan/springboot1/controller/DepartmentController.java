056901eb4ad01f4a0d6e32fbaf142434fd3f2633
Chetan P Hattoor <chetanhattoor@gmail.com>
2023-07-03 00:39:06 +0530

    added logger example

--- src/main/java/com/chetan/springboot1/controller/DepartmentController.java
+++ src/main/java/com/chetan/springboot1/controller/DepartmentController.java
@@ -3,6 +3,7 @@ package com.chetan.springboot1.controller;
 import com.chetan.springboot1.entity.Department;
 import com.chetan.springboot1.service.DepartmentService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,13 +16,28 @@ public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
 
+    //SL4J (Simple Logging Facade for Java) is a Java logging framework.
+    // SLF4J is a simple facade for various logging frameworks, such as java.util.logging, logback and log4j.
+    // SLF4J allows the end-user to plug in the desired logging framework at deployment time.
+    // Basically, SLF4J is an abstraction layer between your application and the logging framework.
+    // It provides a common interface to perform logging activities.
+    // The underlying logging framework can be changed without changing the code in your application.
+    // The SLF4J API supports various logging levels such as INFO, DEBUG, WARN, ERROR, and TRACE.
+    // The logging level can be configured externally at deployment time.
+    // The SLF4J API supports various logging frameworks such as Logback, Log4j, and java.util.logging.
+    // You can choose any of the logging frameworks and configure it with SLF4J.
+    //By default, SLF4J is configured with NOP (No Operation) logger.
+    private final Logger LOGGER = org.slf4j.LoggerFactory.getLogger(DepartmentController.class);
+
     @PostMapping("/departments")
     public Department saveDepartment(@Valid @RequestBody Department department){
+        LOGGER.info("Inside saveDepartment of DepartmentController");
         return departmentService.saveDepartment(department);
     }
 
     @GetMapping("/departments")
     public List<Department> getAllDepartments(){
+        LOGGER.info("Inside getAllDepartments of DepartmentController");
         return departmentService.getAllDepartments();
     }
 
