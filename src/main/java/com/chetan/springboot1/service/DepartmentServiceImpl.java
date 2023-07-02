ffe1dd37481b75646d58be1da639ea687e630b17
Chetan P Hattoor <chetanhattoor@gmail.com>
2023-07-02 18:13:38 +0530

    saveDepartment added

--- src/main/java/com/chetan/springboot1/service/DepartmentServiceImpl.java
+++ src/main/java/com/chetan/springboot1/service/DepartmentServiceImpl.java
@@ -1,7 +1,16 @@
 package com.chetan.springboot1.service;
 
+import com.chetan.springboot1.entity.Department;
+import com.chetan.springboot1.repository.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DepartmentServiceImpl implements DepartmentService{
+    @Autowired
+    private DepartmentRepository departmentRepository;
+    @Override
+    public Department saveDepartment(Department department) {
+        return departmentRepository.save(department);
+    }
 }
