ec88e129b242115206d1eec4fa3e601543eb3b93
Chetan P Hattoor <chetanhattoor@gmail.com>
2023-07-03 18:02:06 +0530

    Unit Test Added

--- src/test/java/com/chetan/springboot1/SpringBoot1ApplicationTests.java
+++ src/test/java/com/chetan/springboot1/SpringBoot1ApplicationTests.java
@@ -6,8 +6,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringBoot1ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
+
+	//why mocking?
+	// we don't care about the underlying implementation of the layer. We mock the underlying layer and give it to the layer we are testing.
+	// This can be done using Mockito.
+
+	// we are testing the service layer. So we mock the repository layer.
+    // we are testing the controller layer. So we mock the service layer.
+    // we are testing the repository layer. So we mock the database layer.
+
 
 }
